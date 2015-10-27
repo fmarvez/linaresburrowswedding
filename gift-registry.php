56ccb44f08dd24835e74e09347272918e654106b
nlinares <nlinares623@gmail.com>
2015-10-27 16:35:02 -0400

    Added Macys

--- gift-registry.php
+++ gift-registry.php
@@ -10,14 +10,29 @@ require_once("includes/header.php");
                     <div class="wedding-party-attendant m-bg-large-divider-graphic has-bio has-photo">
                         <div class="text-wrap">
                             <h3>Gift Registry</h3>
-                            <h5>Pottery Barn</h5>
+
                             <p>&nbsp;</p>
-                            <p class="bio">Search for our names on the site<br/>or print our registry in the store.</p>
-                            <p>&nbsp;</p>
-                            <p><a class="btn m-btn-base" href="http://www.potterybarn.com/registry/find-registry.html" target="_blank">View Registry</a></p>
                         </div>
+                        
+                        <div class="text-wrap">
+                            <h5>Pottery Barn</h5>
+                            <p class="bio">Search for our names on the site<br/>or print our registry in the store.</p>
+                            <p><a class="btn m-btn-base" href="http://www.potterybarn.com/registry/find-registry.html" target="_blank">View Registry</a></p> 
+                        </div>
+                        
                         <div class="photo">
-                            <img src="media/PotteryBarn.jpg" alt="Pottery Barn">
+                            <img src="media/PotteryBarn.jpg" alt="Pottery Barn" style="width:150px">
+                                <p>&nbsp;</p>
+                        </div>
+                        
+                        <div class="text-wrap">
+                            <h5>Macy's</h5>
+                            <p class="bio">Search for our names on the site<br/>or print our registry in the store.</p>
+                            <p><a class="btn m-btn-base" href="https://www.macys.com/wedding-registry?cm_sp=global_nav_reg-_-wedding_registry-_-n" target="_blank">View Registry</a></p>
+                        </div>
+                        
+                        <div class="photo">
+                            <img src="media/Macys.jpg" alt="Macy's" style="width:150px">
                         </div>
                     </div>
                 </div>
