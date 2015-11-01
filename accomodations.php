d8c2f3bcad604c8adc7391f1f2fa1378317a4cfb
nlinares <nlinares623@gmail.com>
2015-11-01 18:31:47 -0500

    Hotel - online booking

--- accomodations.php
+++ accomodations.php
@@ -13,9 +13,10 @@ require_once("includes/header.php");
                             <h5>Renaissance Meadowlands Hotel</h5>
                             <p>&nbsp;</p>
                             <p class="bio">Rooms have been reserved at the<br/>Renaissance Meadowlands Hotel.</p>
-                            <p class="bio">Reference the Linares-Burrows wedding<br/>to receive a special rate by January 26th.</p>
+                            <p class="bio">801 Rutherford Avenue<br/>Rutherford, New Jersey 07070</p>
                             <p>&nbsp;</p>
-                            <p class="bio">801 Rutherford Avenue<br/>Rutherford, New Jersey 07070<br/>(201) 231-3100</p>
+                            <p class="bio">Reference the Linares-Burrows wedding<br/>to receive a special rate by January 26th.</p>               
+                            <p class="bio">Call (201) 231-3100 or <a style="text-decoration:underline;" target="blank" href="http://www.marriott.com/meeting-event-hotels/group-corporate-travel/groupCorp.mi?resLinkData=Linares%20%26%20Burrows%20Wedding%5Eewrbr%60lbwlbwa%60157%60USD%60false%604%602/26/16%602/27/16%601/27/16&app=resvlink&stop_mobi=yes">book online</a>.</p>
                             <p>&nbsp;</p>
                             <p class="bio">Transportation will be provided to and from<br/>Nanina's In The Park and the hotel.</p>
                         </div>
