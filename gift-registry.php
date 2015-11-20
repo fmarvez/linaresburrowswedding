3a78a988e66b00adf0c7e6381553e0f7ab9fdb6c
nlinares <nlinares623@gmail.com>
2015-11-20 12:10:47 -0500

    Gift Registry - added C&B

--- gift-registry.php
+++ gift-registry.php
@@ -16,23 +16,35 @@ require_once("includes/header.php");
                         
                         <div class="text-wrap">
                             <h5>Pottery Barn</h5>
-                            <p class="bio">Browse on the Pottery Barn website<br/>or print our registry in the store.</p>
+                            <p class="bio">Browse on the Pottery Barn website<br/>or print our registry in store.</p>
                             <p><a class="btn m-btn-base" href="http://www.potterybarn.com/registry/3778403/registry-list.html" target="_blank">View Online Registry</a></p> 
                         </div>
                         
                         <div class="photo">
                             <img src="media/PotteryBarn.jpg" alt="Pottery Barn" style="width:150px">
-                                <p>&nbsp;</p>
+                            <p>&nbsp;</p>
                         </div>
                         
                         <div class="text-wrap">
                             <h5>Restoration Hardware</h5>
-                            <p class="bio">Browse on the Restoration Hardware website<br/>or print our registry in the store.</p>
+                            <p class="bio">Browse on the Restoration Hardware website<br/>or print our registry in store.</p>
                             <p><a class="btn m-btn-base" href="https://www.restorationhardware.com/storefront/gift-registry/gift-list.jsp?giftlistId=gl1024901183" target="_blank">View Online Registry</a></p>
                         </div>
                         
                         <div class="photo">
                             <img src="media/RestorationHardware.jpg" alt="Restoration Hardware" style="width:150px">
+                            <p>&nbsp;</p>
+                        </div>
+                        
+                        <div class="text-wrap">
+                            <h5>Crate & Barrel</h5>
+                            <p class="bio">Browse on the Crate & Barrel website<br/>or print our registry in store.</p>
+                            <p><a class="btn m-btn-base" href="http://www.crateandbarrel.com/gift-registry/rebecca-linares-and-nick-burrows/r5429637" target="_blank">View Online Registry</a></p>
+                        </div>
+                        
+                        <div class="photo">
+                            <img src="media/CrateBarrel.png" alt="Crate & Barrel" style="width:150px">
+                            <p>&nbsp;</p>
                         </div>
                     </div>
                 </div>
